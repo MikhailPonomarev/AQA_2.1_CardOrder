f120ecbd04f75719de2237c986b331d7a5aa5e31
MikhailPonomarev <michailp800@gmail.com>
2021-10-06 17:20:01 +0300

    Added happy path tests

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -1,2 +1,56 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CardOrderTest {
+    public WebDriver driver;
+
+    @BeforeAll
+    public static void setupWebDriverManager() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void setUp () {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+
+    @Test
+    public void HappyPathTestV1() {
+        driver.get("http://localhost:9999");
+        List<WebElement> inputField = driver.findElements(By.className("input__control"));
+        inputField.get(0).sendKeys("Иванов Иван");
+        inputField.get(1).sendKeys("+79001000110");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+        String actual = driver.findElement(By.tagName("p")).getText();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void HappyPathTestV2() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Ива-нов Иван");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79001000110");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("[role='button']")).click();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expected, actual.strip());
+    }
 }
